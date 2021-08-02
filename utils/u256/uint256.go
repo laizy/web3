18d442ccdae2621f59d5cea9ba5ca7140de9eee3
laizy <aochyi@126.com>
2021-08-02 10:39:27 +0800

    add decode tx input function
    (cherry picked from commit 4b9415b55c7318610a9a216208faa1df8249a2a4)

--- utils/u256/uint256.go
+++ utils/u256/uint256.go
@@ -30,9 +30,26 @@ func New(v interface{}) Int {
 		return Int{val}
 	case big.Int:
 		return Int{&val}
+	case uint256.Int:
+		return Int{val.ToBig()}
+	case *uint256.Int:
+		return Int{val.ToBig()}
+	case Int:
+		return val
+	case *Int:
+		return *val
 	default:
 		panic("")
 	}
+}
+
+func Add(val ...interface{}) Int {
+	result := New(0)
+	for _, v := range val {
+		result = result.Add(New(v))
+	}
+
+	return result
 }
 
 func Mul(val ...interface{}) Int {
@@ -51,6 +68,10 @@ func (self Int) Mul(val Int) Int {
 
 func (self Int) Add(val Int) Int {
 	return Int{big.NewInt(0).Add(self.val, val.val)}
+}
+
+func Sub(val1, val2 interface{}) Int {
+	return New(val1).Sub(New(val2))
 }
 
 func (self Int) Sub(val Int) Int {
@@ -100,4 +121,43 @@ func (self Int) LessThan(rhs Int) bool {
 
 func (self Int) String() string {
 	return self.val.String()
+}
+
+func (self Int) ToFixNum(precise uint64) string {
+	return toStringByPrecise(self.ToBigInt(), precise)
+}
+
+func toStringByPrecise(bigNum *big.Int, precise uint64) string {
+	result := ""
+	destStr := bigNum.String()
+	destLen := uint64(len(destStr))
+	if precise >= destLen { // add "0.000..." at former of destStr
+		var i uint64 = 0
+		prefix := "0."
+		for ; i < precise-destLen; i++ {
+			prefix += "0"
+		}
+		result = prefix + destStr
+	} else { // add "."
+		pointIndex := destLen - precise
+		result = destStr[0:pointIndex] + "." + destStr[pointIndex:]
+	}
+	result = removeZeroAtTail(result)
+	return result
+}
+
+// delete no need "0" at last of result
+func removeZeroAtTail(str string) string {
+	i := len(str) - 1
+	for ; i >= 0; i-- {
+		if str[i] != '0' {
+			break
+		}
+	}
+	str = str[:i+1]
+	// delete "." at last of result
+	if str[len(str)-1] == '.' {
+		str = str[:len(str)-1]
+	}
+	return str
 }
