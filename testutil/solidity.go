0c76aa3c5894360fd75c64efcfb7c92eb59a0145
laizy <aochyi@126.com>
2021-09-13 11:01:03 +0800

    clean

--- testutil/solidity.go
+++ testutil/solidity.go
@@ -1,14 +1,12 @@
 package testutil
 
 import (
-	"fmt"
 	"os/exec"
 )
 
 func IsSolcInstalled() bool {
 	output, err := exec.Command("solc", "--version").Output()
 	if err != nil {
-		fmt.Println(err.Error())
 		return false
 	}
 
