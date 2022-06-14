bb1a008559dfd53127a82e6df9e7df0ca81d276c
laizy <aochyi@126.com>
2022-06-14 18:49:49 +0800

    more chain call and add bytes endian function (#46)
    * support chain call
    
    * more bytes endian function

--- utils/codec/codec.go
+++ utils/codec/codec.go
@@ -1,5 +1,7 @@
 package codec
 
+import "encoding/binary"
+
 type Serializable interface {
 	Serialization(sink *ZeroCopySink)
 }
@@ -11,4 +13,34 @@ func SerializeToBytes(values ...Serializable) []byte {
 	}
 
 	return sink.Bytes()
+}
+
+func BytesLEToUint16(data []byte) uint16 {
+	return binary.LittleEndian.Uint16(data)
+}
+
+func Uint16ToBytesLE(val uint16) []byte {
+	data := make([]byte, 2)
+	binary.LittleEndian.PutUint16(data, val)
+	return data
+}
+
+func BytesLEToUint32(data []byte) uint32 {
+	return binary.LittleEndian.Uint32(data)
+}
+
+func Uint32ToBytesLE(val uint32) []byte {
+	data := make([]byte, 4)
+	binary.LittleEndian.PutUint32(data, val)
+	return data
+}
+
+func BytesLEToUint64(data []byte) uint64 {
+	return binary.LittleEndian.Uint64(data)
+}
+
+func Uint64ToBytesLE(val uint64) []byte {
+	data := make([]byte, 8)
+	binary.LittleEndian.PutUint64(data, val)
+	return data
 }
