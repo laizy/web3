25de9dcfe65537f6efc6aebd74f69dab6e6fe4b2
XuDon9 Liu AKA Unknown Rapper Of CN background diablo & revelations, a man optimistic, nagetive, silent, active, Taoism <1027309561@qq.com>
2022-06-20 17:09:55 +0800

    jsonrpc/l2.go: add GlobalInfo api

--- jsonrpc/l2.go
+++ jsonrpc/l2.go
@@ -17,3 +17,29 @@ func (l *L2) GetPendingTxBatches() ([]byte, error) {
 	err := l.c.Call("l2_getPendingTxBatches", &out)
 	return out, err
 }
+
+type InputChainInfo struct {
+	PendingQueueIndex uint64
+	TotalBatches      uint64
+	QueueSize         uint64
+}
+type GlobalInfo struct {
+	//total batch num in l1 RollupInputChain contract
+	L1InputInfo InputChainInfo
+	//l2 client have checked tx batch num
+	L2CheckedBatchNum uint64
+	//the total block num l2 already checked,start from 1, because genesis block do not need to check
+	L2CheckedBlockNum uint64
+	//l2 client head block num
+	L2HeadBlockNumber   uint64
+	L1SyncedBlockNumber uint64
+	L1SyncedTimestamp   *uint64
+}
+
+//tx batch data is already encoded as params of AppendBatch in RollupInputChain.sol, just add a func selector beyond it
+//to invoke the AppendBatch is fine.
+func (l *L2) GlobalInfo() (*GlobalInfo, error) {
+	var out GlobalInfo
+	err := l.c.Call("l2_globalInfo", &out)
+	return &out, err
+}
